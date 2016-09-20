66cee7114cac67c2fa9a40ae0d3b5eb2c9c65d11
michael woo <mwoo123@gmail.com>
2016-09-20 19:50:08 -0400

    added express to bot

--- kokonatsuBotDiscordjs.js
+++ kokonatsuBotDiscordjs.js
@@ -1,3 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 var Discord = require("discord.js");
 var request = require('request');
 
