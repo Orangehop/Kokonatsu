fe4d17becafcf84d43154f559c0560608765ed5a
michael woo <mwoo123@gmail.com>
2016-10-14 22:39:03 -0400

    updated help messages

--- bot/helpMessages.js
+++ bot/helpMessages.js
@@ -6,9 +6,11 @@ var configHelp = "```== COMMANDS LIST ==```\n"+
                              "**delete** | **remove** `\<macro name> <macro number, default = 1>` - delete/remove a macro\n"+
                              "**list** - gives link to kokonatsu macro site\n"+
                              "**rename** `\<macro name> \<new macro name>` - renames all of one macro to another\n"+
-                             "**top** `\<type, number, default = 10>` - set type to either 'score' or 'usage' to get the top scored or used macros\n"+
+                             "**top** `\<type> \<number, default = 10>` - set type to either 'score' or 'usage' to get the top scored or used macros\n"+
                              "**usage** `\<macro name> \<macro number, optional>` - determine how often a macro has been used. Gets the usage of all named macros if no number given\n"+
-                             "**score** `\<macro name> \<macro number, optional>` - returns the score of a macro. if no number is given then return net macro score\n";
+                             "**score** `\<macro name> \<macro number, optional>` - returns the score of a macro. if no number is given then return net macro score\n"+
+                             "**vote** `\<up || down> \<macro name> \<macro number>` - up/down vote a macro to affect it's score\n"
+                             ;
 
 module.exports.quickHelp = quickHelp;
 module.exports.configHelp = configHelp;
